7361adb28f9a7e89c9d9766f2f80192a285242d3
Seema-476 <simmybishnoi29085@gmail.com>
2024-10-02 12:47:41 +0530

    add href id in nav buttons with section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // menu-icon
 function opennav() {
-    document.querySelector(".menulist").classList.toggle("start-0");
+    document.getElementById("navlist").classList.toggle("start-0");
     document.querySelector(".menuicon").classList.toggle("cross");
     document.body.classList.toggle("overflow-hidden");
 }
